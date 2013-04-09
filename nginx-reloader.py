44a913202d4fc153b14d9a0429b13b3111ab73ed
Andrew Rowson <github@growse.com>
2013-04-09 14:04:10 +0100

    Now finds and HUPs nginx master

--- nginx-reloader.py
+++ nginx-reloader.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 
+import psutil
 import pyinotify
-
+import os
+import signal
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_default(self, event):
         if event.name.endswith(".conf"):
-            print event
+            pid = get_nginx_pid()
+            os.kill(pid, signal.SIGHUP)
+            print 'Sent HUP to {}'.format(pid)
+
+
+def get_nginx_pid():
+    return [p.pid for p in psutil.process_iter() if "nginx: master process" in str(p.cmdline)][0]
 
 wm = pyinotify.WatchManager()
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
-mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_CLOSE_WRITE
+mask = pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE
 wm.add_watch('/var/www', mask, rec=True)
 notifier.loop()
